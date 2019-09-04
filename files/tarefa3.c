79db54747c2682c0de06b27aadcbd5c20bbede9e
lucasokada <lucas.okada2000@gmail.com>
2019-09-03 23:44:53 -0300

    tarefa 3 v2

--- files/tarefa3.c
+++ files/tarefa3.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*fprintf e usado para ESCREVER uma lista formatada de variaveis em um arquivo 
     fprintf(file, "tipo de saida", variaveis); */
@@ -25,7 +25,6 @@ void menu()
     printf("-----------------------------------\n");
 }
 
-
 int escolha()
 {
     int escolha;
@@ -33,105 +32,173 @@ int escolha()
     scanf("%d", &escolha);
 }
 
-void verifica_nota(struct tipo_matricula_aluno matricula_aluno[], int count)
+/*void verifica_nota(struct tipo_matricula_aluno matricula_aluno[], int count)
 {
     
+}*/
+
+int validar_nota(float nota)
+{
+    return nota < 0 || nota > 10 ? 0 : 1;
 }
 
-void cadastra_aluno(struct tipo_matricula_aluno matricula_aluno[], int *count)
+struct tipo_matricula_aluno montar_aluno(int matricula_digitada)
 {
-    //int count=0;
-    FILE *file;
-    file = fopen("tarefa3.txt", "w"); //cria / acessa arquivo com permissao de escrita
+    struct tipo_matricula_aluno aluno;
+    float nota;
 
-     do
+    aluno.matricula = matricula_digitada;
+
+    printf("digite o nome do aluno: \n");
+    scanf(" %[^\n]s", aluno.nome_aluno);
+
+    printf("digite a nota 1 do aluno: \n");
+    do
     {
-        printf("digite a matricula do aluno %d:\n", *count+1);
-        scanf("%d", &matricula_aluno[*count].matricula);
+        scanf("%f", &nota);
 
-        if(matricula_aluno[*count].matricula != -1){
-        
-            fprintf(file, "matricula %d: ", *count+1);
-            fprintf(file, "%d\n", matricula_aluno[*count].matricula);
-
-            fprintf(file, "nome do aluno: ");
-            printf("digite o nome do aluno:\n");
-            scanf(" %[^\n]s", matricula_aluno[*count].nome_aluno);
-            fprintf(file, "%s\n", matricula_aluno[*count].nome_aluno);
-
-            printf("digite a nota 1 do aluno:\n");
-            fprintf(file, "nota1 do aluno: ");
-            do{
-                scanf("%f", &matricula_aluno[*count].nota1);    //verifica e registra nota 1
-                if(matricula_aluno[*count].nota1 < 0 || matricula_aluno[*count].nota1 > 10){
-                    printf("nota invalida.tente novamente!\n");
-                }
-            }while((matricula_aluno[*count].nota1 < 0) || (matricula_aluno[*count].nota1 > 10));
-            fprintf(file, "%.2f\n", matricula_aluno[*count].nota1);
-
-            printf("digite a nota 2 do aluno:\n");
-            fprintf(file, "nota2 do aluno: ");
-            do{
-                scanf("%f", &matricula_aluno[*count].nota2);    //verifica e registra nota 2
-                if(matricula_aluno[*count].nota2 < 0 || matricula_aluno[*count].nota2 > 10){
-                    printf("nota invalida.tente novamente!\n");
-                }
-            }while((matricula_aluno[*count].nota2 < 0) || (matricula_aluno[*count].nota2 >10));
-            fprintf(file, "%.2f\n", matricula_aluno[*count].nota2);
-
-            (*count)++;
+        if (!validar_nota(nota)){
+            printf("nota invalida.tente novamente!\n");
+        }else{
+            aluno.nota1 = nota;
         }
 
+    } while (!validar_nota(nota));
 
-    } while (*count < 10 && matricula_aluno[*count].matricula != -1);
-    
-    fclose(file);
-    printf("%d", *count);
+    printf("digite a nota 2 do aluno: \n");
+    do
+    {
+        scanf("%f", &nota);
+
+        if (!validar_nota(nota)){
+            printf("nota invalida.tente novamente!\n");
+        }else{
+            aluno.nota2 = nota;
+        }
+
+    } while (!validar_nota(nota));
+
+    return aluno;
 }
 
-int verifica_existencia(struct tipo_matricula_aluno matricula_aluno[], int count)
+void gravar_aluno_arquivo(struct tipo_matricula_aluno aluno)
+{
+    FILE *file;
+    file = fopen("tarefa3.txt", "a"); //cria / acessa arquivo com permissao de escrita
+
+    fprintf(file, "%d", aluno.matricula);
+    fprintf(file, " %s", aluno.nome_aluno);
+    fprintf(file, " %f", aluno.nota1);
+    fprintf(file, " %f \n", aluno.nota2);
+
+    fclose(file);
+}
+
+int cadastra_aluno(struct tipo_matricula_aluno matricula_aluno[])
+{
+    int quantidade_inserida = 0, matricula_digitada = 0;
+    
+    do
+    {
+        printf("digite a matricula do aluno %d:\n", quantidade_inserida + 1);
+        scanf("%d", &matricula_digitada);
+
+        if (matricula_digitada != -1)
+        {
+            matricula_aluno[quantidade_inserida] = montar_aluno(matricula_digitada);
+            gravar_aluno_arquivo(matricula_aluno[quantidade_inserida]);
+
+            quantidade_inserida++;
+        }
+
+    } while (quantidade_inserida < 10 && matricula_digitada != -1);
+
+    printf("quantidade inserida %d\n", quantidade_inserida);
+
+    return quantidade_inserida;
+}
+
+
+int verifica_existencia()
 {
     int matricula_desejada;
-    int achou, i, posicao;
+    int achou = 0, posicao = -1;
+    struct tipo_matricula_aluno aluno;
 
     FILE *file;
-
     file = fopen("tarefa3.txt", "r");
 
     printf("digite a matricula do aluno desejado:\n");
     scanf("%d", &matricula_desejada);
 
-    while(fscanf(file, ))
-        {
-        achou = matricula_desejada == matricula_aluno[i].matricula ? 1 : 0;
-        if(achou == 1){
-            posicao = i;
-            //printf("existe\n");
+    while (achou == 0 && fscanf(file, "%d %s %f %f \n", &aluno.matricula, aluno.nome_aluno, &aluno.nota1, &aluno.nota2) != EOF)
+    {
+        posicao++;
+        achou = matricula_desejada == aluno.matricula ? 1 : 0;
+    }
+
+    fclose(file);
+
+    return achou == 1 ? posicao : -1;
+}
+
+void posiciona_arquivo(int numero_linha, FILE *file)
+{
+    char c;
+    int quantidade_linha = 0;
+    fseek(file, 0, SEEK_SET);
+    while(quantidade_linha < numero_linha  && fscanf(file, "%c", &c))
+    {
+        printf("%c ", c);
+        if(c == '\n'){
+            quantidade_linha++;
         }
     }
-    if(achou == 0){
-            printf("matricula nao encontrada. tente novamente!\n");
-        }
-
-    return posicao;
+    
 }
 
-void mudar_notas(struct tipo_matricula_aluno matricula_aluno[], int count)
+void mudar_notas()
 {
-    verifica_existencia(matricula_aluno, count);
+    int existe = verifica_existencia();
+    struct tipo_matricula_aluno aluno;
 
+    if (existe != -1)
+    {
+        FILE *file;
+        file = fopen("tarefa3.txt", "r+");
 
+        posiciona_arquivo(existe, file);
+        
+        fscanf(file, "%d %s %f %f", &aluno.matricula, aluno.nome_aluno, &aluno.nota1, &aluno.nota2);
+        printf("%d %s %f %f \n", aluno.matricula, aluno.nome_aluno, aluno.nota1, aluno.nota2);
+
+        posiciona_arquivo(existe, file);
+
+        aluno.nota1 = 4;
+
+        fprintf(file, "%d %s %f %f", aluno.matricula, aluno.nome_aluno, aluno.nota1, aluno.nota2);
+
+        fclose(file);
+    }
+    else
+    {
+        printf("Aluno nao encontrado! \n");
+
+    }
 }
-
 
 int main()
 {
-    int tamanho = 10;
+    int teste;
+    char nome[20];
+    fscanf(stdin, "%d", &teste);
+    //fgets(nome, 20, stdin);
+    //printf("\n nome %s\n", nome);
+    fprintf(stdout, "\nvalor digitade %d\n", teste);
+    /*int tamanho = 10;
     struct tipo_matricula_aluno matricula_aluno[tamanho];
     int count = 0, matricula_desejada;
-
-    cadastra_aluno(matricula_aluno, &count);
-    mudar_notas(matricula_aluno, count);
-
+*/
+    //cadastra_aluno(matricula_aluno);
+    //mudar_notas();
 }
-
